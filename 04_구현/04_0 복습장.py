13ad54a4293ef03bd51d0a1d7d9da92e4910d9da
YUL <hramsm@naver.com>
2023-02-21 00:18:59 +0900

    Update 04_0 복습장.py

--- 04_구현/04_0 복습장.py
+++ 04_구현/04_0 복습장.py
@@ -26,5 +26,69 @@ for i in range(h+1):
 print(count)
 
 # 왕실의 나이트
+input_data = input()
+row = int(input_data[1])
+column = int(ord(input_data[0])) - int(ord('a')) + 1
+
+
 result = 0
-for step in steps
+for step in steps:
+    next_row = row + step[0]
+    next_column = column + step[1]
+    if next_row >= 1 and next_row <= 8 and next_column >= 1 and next_column <= 8:
+        result += 1
+print(result)
+
+# 게임 개발
+1. 회전하는 방법
+2. 반복은 육지가 없을 때까지
+2. 바라보는 방향의 앞이 육지라면 이동 +1, 방문한 육지는 1로 변경/ 육지가 아니라면 다시 회전
+3. 1~2 반복하면서 총 이동 횟수 저장
+
+n,m = map(int, input().split())
+d = [[0] * m for _ in range(n)]
+x,y,direction = map(int, input().split())
+d[x][y] = 1
+
+array = []
+for i in range(n):
+    array.append(list(map(int, input().split())))
+
+dx = [-1,0,1,0]
+dy = [0,1,0,-1]
+
+def turn_left():
+    global direction
+    direction -= 1
+    if direction == -1:
+        direction = 3
+
+count = 1
+turn_time = 0
+while true:
+    turn_left()
+    nx = x + dx[direction]
+    ny = y + dy[direction]
+    if d[nx][ny] == 0 and array[nx][ny] == 0:
+        d[nx][ny] = 1
+        x = nx
+        y = ny
+        count += 1
+        turn_time = 0
+        continue
+    else:
+        turn_time += 1
+    if turn_time == 4:
+        nx = x - dx[direction]
+        ny = y - dy[direction]
+        if array[nx][ny] == 0:
+            x = nx
+            y = ny
+        else:
+            break
+        turn_time = 0
+print(count)
+
+
+
+
